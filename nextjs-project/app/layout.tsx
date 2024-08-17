d5a1b3c1384d10b82cf5555264cdcdb08e757924
KarTaGo124 <joseguillermogalvezpacori@gmail.com>
2024-08-17 18:44:48 -0500

    nextjs-project initial commit

---
+++ nextjs-project/app/layout.tsx
@@ -0,0 +1,19 @@
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<head>
+				<title>My App</title>
+			</head>
+			<body>
+				<main>{children}</main>
+			</body>
+		</html>
+	);
+}
